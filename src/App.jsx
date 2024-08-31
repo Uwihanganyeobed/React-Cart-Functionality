b630f349ff8a249338e73931fa318d62959720b8
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-31 10:07:32 +0200

    removing unwanted contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+
 import Products from './components/Products'
 import { CartProvider } from './context/cart'
 
 const App = () => {
+
   return (
     <CartProvider>
       <Products/>
